6cab6b7caf6af8418b996ac0a94df4ade2d548ad
Wesley Rockholz <wesley.rockholz@gmail.com>
2019-01-26 17:01:03 -0800

    Add start screen

---
+++ Assets/Scripts/Components/UI/StartScreen/StartScreenController.cs
@@ -0,0 +1,15 @@
+﻿using Game.Attributes;
+using Game.Components.UI.Abstract;
+using Game.UnityExtensions.Scenes;
+
+namespace Game.Components.UI.StartScreen
+{
+    public sealed class StartScreenController : UIMonoBehaviour
+    {
+        [UnityEventBinding]
+        public void OnPressPlay()
+        {
+            SceneFunctions.TransitionScene("StartScreen", "Gameplay");
+        }
+    }
+}
